79ef74a7ad360169f8e4bb13490c57c8bce77bd5
bhobbs <bry.hobbs@gmail.com>
2016-02-25 20:08:03 -0500

    converting tabs to spaces

--- distinct_powers/distinct_powers.rb
+++ distinct_powers/distinct_powers.rb
@@ -1,10 +1,10 @@
 
 def distinct_powers(min, max)
-	powers = []
-	(min..max).each do |base| 
-		(min..max).each { |exponent| powers << base**exponent } 
-	end
-	powers.uniq.length
+    powers = []
+    (min..max).each do |base| 
+        (min..max).each { |exponent| powers << base**exponent } 
+    end
+    powers.uniq.length
 end
 
 puts distinct_powers(2, 5)
--- rspec_quiz/spec_quiz.rb
+++ rspec_quiz/spec_quiz.rb
@@ -2,28 +2,28 @@ require_relative 'quiz'
 
 describe HtmlDocument do
 
-	describe '#meta_keywords' do
-		let(:parser) { double }
-		before { @doc = HtmlDocument.new(parser)}
+    describe '#meta_keywords' do
+        let(:parser) { double }
+        before { @doc = HtmlDocument.new(parser)}
 
-		it 'gets meta keywords' do
-			expect(parser).to receive(:get_meta_tag)
-			@doc.meta_keywords
-		end
+        it 'gets meta keywords' do
+            expect(parser).to receive(:get_meta_tag)
+            @doc.meta_keywords
+        end
 
-		it 'returns content' do
-			tag = double
-			allow(tag).to receive(:attribute).and_return("hello world")
-			allow(parser).to receive(:get_meta_tag).and_return(tag)
-			expect(@doc.meta_keywords).to eql(["hello world"])
-		end
+        it 'returns content' do
+            tag = double
+            allow(tag).to receive(:attribute).and_return("hello world")
+            allow(parser).to receive(:get_meta_tag).and_return(tag)
+            expect(@doc.meta_keywords).to eql(["hello world"])
+        end
 
-		it 'strips and splits content' do
-			tag = double
-			allow(tag).to receive(:attribute).and_return("hello world, hi       ")
-			allow(parser).to receive(:get_meta_tag).and_return(tag)
-			expect(@doc.meta_keywords).to eql(["hello world", "hi"])			
-		end
+        it 'strips and splits content' do
+            tag = double
+            allow(tag).to receive(:attribute).and_return("hello world, hi       ")
+            allow(parser).to receive(:get_meta_tag).and_return(tag)
+            expect(@doc.meta_keywords).to eql(["hello world", "hi"])            
+        end
 
-	end
+    end
 end
